4105dfea3113087397613f759fa43dbdebe1d218
Pavandkurdekar <pavandkurdekar55@gmail.com>
2023-09-08 13:44:16 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => { 
+    return (
+        <div>
+           <h1>iNotebook is a feature-packed note-taking app that offers a range of tools for capturing and organizing your ideas, information, and memories. With its user-friendly interface and intuitive design, iNotebook is the perfect tool for anyone looking to simplify their note-taking process.</h1>
+        </div>
+    )
+}
+
+export default About
